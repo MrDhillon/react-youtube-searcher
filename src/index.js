fad0aa52829d8464951441589d279b147facd373
MrDhillon <harman.singh.dhillon@gmail.com>
2016-01-17 04:12:49 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyC6p8q8Lj_dE56KARa2Cs7J0UfUtgDr7u4';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// create new component. This should produce html
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    YTSearch({key: API_KEY, term: 'starwars'}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideos: videos[0]
+      });
+      // this.state({videos: videos});
+    });
+  }
+
+  render(){
+    return (
+      <div>
+        <SearchBar/>
+        <VideoDetail video={this.state.selectedVideos}/>
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+// Take this generated html and put it in the DOM
+
+ReactDOM.render(<App />, document.querySelector('.container'));
